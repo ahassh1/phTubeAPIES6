15bf9d278cdf2858c6c436b1389d9171f3fd12d6
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-04-10 23:34:04 +0600

    ph tube fully done

--- script/app.js
+++ script/app.js
@@ -1,4 +1,12 @@
-
+console.log("file is connected");
+const showLoader = () => {
+  document.getElementById("loader").classList.remove("hidden");
+  document.getElementById("video-container").classList.add("hidden");
+};
+const hideLoader = () => {
+  document.getElementById("loader").classList.add("hidden");
+  document.getElementById("video-container").classList.remove("hidden");
+};
 function removeActiveClass() {
   const activeButtons = document.getElementsByClassName("active");
   for (let btn of activeButtons) {
@@ -6,151 +14,222 @@ function removeActiveClass() {
   }
 }
 
-
-
-
 function loadCategories() {
-  // fetch the data
+  // 1- fetch the data
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    // convert promise to json
-    .then(res => res.json())
-    // send data to display+
-    .then(data => displayCategories(data.categories));
+    //2 - convert promise to json
+    .then((res) => res.json())
+    //3 - send data to display
+    .then((data) => displayCategories(data.categories));
 }
 
+function loadVideos(searchText = "") {
+  showLoader();
+  fetch(
+    `https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      removeActiveClass();
+      document.getElementById("btn-all").classList.add("active");
+      displayVideos(data.videos);
+    });
+}
+//1001
+const loadCategoryVideos = (id) => {
+  showLoader();
+  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+  console.log(url);
 
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      removeActiveClass();
+      //no active class
+
+      const clickedButton = document.getElementById(`btn-${id}`);
+      clickedButton.classList.add("active");
+
+      displayVideos(data.category);
+    });
+};
+
+// video card 
+const loadVideoDetails = (videoId) => {
+  console.log(videoId);
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideoDetails(data.video));
+};
+
+const displayVideoDetails = (video) => {
+  console.log(video);
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details-container");
+
+  detailsContainer.innerHTML = `
+   <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and
+     inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      
+    </div>
+  </div>
+</div>
+  `;
+};
+
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+// }
 function displayCategories(categories) {
-  // Get the container
+  // get the container
   const categoryContainer = document.getElementById("category-container");
-  
-  // Loop over categories
+
+  // Loop operation on Array of object
   for (let cat of categories) {
     // console.log(cat);
-    
-    // Create and populate the category element inside the loop
+
+    // create Element
     const categoryDiv = document.createElement("div");
+
     categoryDiv.innerHTML = `
-    <button id="btn-${cat.category_id}" onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-red-500 hover:text-white">${cat.category}</button>
+    <button id="btn-${cat.category_id}" 
+     onclick="loadCategoryVideos(${cat.category_id})" 
+     class="btn btn-sm hover:bg-[#FF1F3D] 
+      hover:text-white">${cat.category}</button>
     `;
-    
-    // Append the element
+
+    // Append the Element
     categoryContainer.append(categoryDiv);
   }
 }
 
-function loadVideos(){
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
-  .then(res => res.json())
-  .then(data=>{
-    removeActiveClass()
-   
-    document.getElementById('btn-all').classList.add('active')
+// {
+//     "category_id": "1003",
+//     "video_id": "aaak",
+//     "thumbnail": "https://i.ibb.co/ZNggzdm/cake.jpg",
+//     "title": "Beyond The Pale",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/MZ2vbXR/jimm.jpg",
+//             "profile_name": "Jim Gaffigan",
+//             "verified": false
+//         }
+//     ],
+//     "others": {
+//         "views": "2.6K",
+//         "posted_date": "15400"
+//     },
+//     "description": "'Beyond The Pale' by Jim Gaffigan, 
+// with 2.6K views, is a comedic gem that explores 
+// everyday observations and family life with a 
+// light-hearted and witty approach. Jim's humor is 
+// accessible and delightful, making this show perfect 
+// for anyone who enjoys clean, observational comedy."
+// }
 
-    displayVideos(data.videos)
-  })
-}
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("video-container");
 
+  videoContainer.innerHTML = "";
 
-
-
-const displayVideos =(videos)=>{
-   const videoContainer = document.getElementById('video-container');
-   videoContainer.innerHTML="";
-
-   if(videos.length== 0){
-    videoContainer.innerHTML =`
-      <div class="py-20 col-span-full flex flex-col justify-center items-center ">
-    <img class="" src="./assets/Icon.png" alt="">
-    <h2 class="text-2xl font-bold">Oops!! Sorry, There is no content here</h2>
-   </div>
+  if (videos.length == 0) {
+    videoContainer.innerHTML = `
+    <div
+        class="py-20 col-span-full flex flex-col 
+        justify-center items-center text-center"
+      >
+        <img class="w-[120px]" src="./assets/Icon.png" alt="" />
+        <h2 class="text-2xl font-bold">
+          Oops!! Sorry, There is no content here
+        </h2>
+      </div>
     `;
+    hideLoader();
     return;
-   }
+  }
 
-   videos.forEach(video =>{
-    // console.log(video)
-    const videoDiv = document.createElement('div')
-    videoDiv.innerHTML=`
+  videos.forEach((video) => {
+    // console.log(video);
 
-    <!-- //card section design start -->
-    <div class="card bg-base-100 shadow-sm">
-      <figure class="relative">
-        <img class="w-full h-[160px] object-cover"
-          src="${video.thumbnail}"
-          alt="Shoes" />
-          <span class="absolute bottom-2 right-3 bg-black text-white px-2 rounded-sm text-xs">3hrs 56 min ago</span>
-      </figure>
-      <div class="flex px-0 gap-3 py-5">
-        <div class="profile">
-          <div class="avatar">
-            <div class="ring-primary ring-offset-base-100 w-7 rounded-full ring ring-offset-2">
-              <img src="${video.authors[0].profile_picture}" />
+    const videoCard = document.createElement("div");
+
+    videoCard.innerHTML = `
+     <div class="card bg-base-100">
+        <figure class="relative">
+          <img class="w-full h-[150px] object-cover" src="${
+            video.thumbnail
+          }" alt="Shoes" />
+          <span
+            class="absolute bottom-2 right-2 text-sm 
+            rounded text-white bg-black px-2"
+            >3hrs 56 min ago</span
+          >
+        </figure>
+
+        <div class="flex gap-3 px-0 py-5">
+          <div class="profile">
+            <div class="avatar">
+              <div
+                class="ring-primary ring-offset-base-100 
+                w-6 rounded-full ring ring-offset-2"
+              >
+                <img
+                  src="${video.authors[0].profile_picture}"
+                />
+              </div>
             </div>
           </div>
+
+          <div class="intro">
+            <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+            <p class="text-sm text-gray-400 flex gap-1">
+             ${video.authors[0].profile_name}
+              ${
+                video.authors[0].verified == true
+                  ? `<img
+                class="w-5 h-5"
+                src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"
+                alt=""
+              />`
+                  : ``
+              }
+            </p>
+            <p class="text-sm text-gray-400">${video.others.views} views</p>
+          </div>
+
         </div>
-        <div class="intro">
-          <h1 class="text-xl font-bold">Building a Winning UX Strategy Using the Kano Model</h1>
-          <p class="text-sm text-gray-400 font-semibold flex gap-1 text-[15px] my-1 ">${video.authors[0].profile_name}
-          <img class="w-5 h-5" src="https://img.icons8.com/?size=64&id=eZo3c88c63il&format=png" alt="">
-        </p>
-        <p class="text-sm text-gray-400 font-semibold" >${video.others.views}</p>
-        </div>
+        <button onclick=loadVideoDetails('${
+          video.video_id
+        }') class="btn btn-block">Show Details</button>
       </div>
-    </div>
+    
+    `;
+    //append
+    videoContainer.append(videoCard);
+  });
+  hideLoader();
+};
 
-    `
-    videoContainer.append(videoDiv)
-   })
-}
-
-
-
-const loadCategoryVideos = (id)=> {
-  const url=`https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
-  console.log(url)
-
-  fetch(url)
-  .then(res => res.json())
-  .then(data =>{
-
-    removeActiveClass();
-
-    const clickedButton = document.getElementById(`btn-${id}`)
-    clickedButton.classList.add('active')
-    console.log(clickedButton)
-    displayVideos(data.category)
-
-  })
-}
+document.getElementById("search-input").addEventListener("keyup", (e) => {
+  const input = e.target.value;
+  loadVideos(input);
+});
 
 loadCategories();
 
-
-
-
-// {
-//   "category_id": "1001",
-//   "video_id": "aaah",
-//   "thumbnail": "https://i.ibb.co/hY496Db/coloer-of-the-wind.jpg",
-//   "title": "Colors of the Wind",
-//   "authors": [
-//       {
-//           "profile_picture": "https://i.ibb.co/6r4cx4P/ethen-clack.png",
-//           "profile_name": "Ethan Clark",
-//           "verified": true
-//       }
-//   ],
-//   "others": {
-//       "views": "233K",
-//       "posted_date": "16090"
-//   },
-//   "description": "Ethan Clark's 'Colors of the Wind' is a vibrant musical exploration that captivates listeners with its rich, expressive melodies and uplifting rhythm. With 233K views, this song is a celebration of nature's beauty and human connection, offering a soothing and enriching experience for fans of heartfelt, nature-inspired music."
-// }
-
-
-
-
-{/* <div class="card bg-base-100 shadow-sm">
+{
+  /* <div class="card bg-base-100 shadow-sm">
 <figure>
   <img
     src="${video.thumbnail}"
@@ -163,4 +242,5 @@ loadCategories();
     <button class="btn btn-primary">Buy Now</button>
   </div>
 </div>
-</div> */}
+</div> */
+}
